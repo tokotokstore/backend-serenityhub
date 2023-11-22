a079fefc67b522512011b8f0e5a7042ba5dbdbe2
Diki Wahyudi <68983872+dkwhyd@users.noreply.github.com>
2023-11-22 13:59:15 +0700

    Update connection.js
    delete string db

--- connection.js
+++ connection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-const mongodbAtlas =
-  'mongodb://serenity:serenitylink@ac-ic7ust3-shard-00-00.rb7mi9g.mongodb.net:27017,ac-ic7ust3-shard-00-01.rb7mi9g.mongodb.net:27017,ac-ic7ust3-shard-00-02.rb7mi9g.mongodb.net:27017/serenity?replicaSet=atlas-munim4-shard-0&ssl=true&authSource=admin';
+const mongodbAtlas = 'link mongodb atlas'
 const localConnection =
-  'mongodb://diki:1234@localhost:27017/serenity?authSource=admin';
+  'mongodb://localhost:27017/serenity?authSource=admin';
 
 mongoose.connect(localConnection, {
   useNewUrlParser: true,
