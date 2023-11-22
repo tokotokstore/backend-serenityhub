f249bb90ce6d37ea7b79b2230ae2bf6711c5b2bf
dkwhyd <dikiwhyd99@gmail.com>
2023-11-22 19:24:13 +0700

    add comment documentation

--- src/auth/router.js
+++ src/auth/router.js
@@ -11,7 +11,6 @@ passport.use(
 router.post('/register', multer().none(), controller.register);
 router.post('/login', multer().none(), controller.login);
 router.get('/me', controller.me);
-router.post('/logout', controller.logout)
-router.get('/users', controller.show);
+router.post('/logout', controller.logout);
 
 module.exports = router;
