5efefbfce9de1d3df609e5d88dddfd63747d526d
dkwhyd <dikiwhyd99@gmail.com>
2023-11-24 14:17:24 +0700

    add urlencoded and json

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ const reportRouter = require('./src/reports/router');
 
 const { decodeToken } = require('./src/auth/middleware');
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' },
+);
 // app.use(function (req, res, next) {
 //   res.status(404).send('resource not found');
 //   next();
 // });
 // app.use(morgan('combined', { stream: accessLogStream }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(decodeToken());
 
 app.use(authRouter);
@@ -28,3 +33,4 @@ app.use(reportRouter);
 app.use(routers);
 
 app.listen(port, () => console.log(`server running at ${port}`));
+module.exports = app;
