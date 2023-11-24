f3896d30fdb54169e40fbc6641b1fbb0020d4b78
D-oxys <sanuzumaki12@gmail.com>
2023-11-24 11:50:04 +0700

    tambah unit test

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ const reportRouter = require('./src/reports/router');
 
 const { decodeToken } = require('./src/auth/middleware');
 
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' },
-);
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 // app.use(function (req, res, next) {
 //   res.status(404).send('resource not found');
 //   next();
@@ -31,3 +28,4 @@ app.use(reportRouter);
 app.use(routers);
 
 app.listen(port, () => console.log(`server running at ${port}`));
+module.exports = app;
