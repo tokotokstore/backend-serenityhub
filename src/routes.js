798bed348c9c22d25bb7613ad25f08f8056e8902
dkwhyd <dikiwhyd99@gmail.com>
2023-11-26 04:12:03 +0700

    menambahkan fitur routing static file

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const app = express();
 
 routers.get('/', (req, res) => res.send('halaman utama'));
 routers.get('/public/download/:name', (req, res) => {
-  const filename = '../public/lampu_jalan_mati.jpeg';
+  const filename = `../public/${req.params.name}`;
   res.download(path.join(__dirname, filename));
 });
 
