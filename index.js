49be642058ecf4e8ddeb9fc108bceea8e702921e
dkwhyd <dikiwhyd99@gmail.com>
2023-11-26 22:32:14 +0700

    add static path

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const routers = require('./src/routes');
 const app = express();
 const createError = require('http-errors');
 const cors = require('cors');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'default')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = 5500;
@@ -18,6 +22,7 @@ const commentRouter = require('./src/comment/router');
 const reportRouter = require('./src/reports/router');
 const uploadImage = require('./src/image/uploadImage');
 const officerReportRouter = require('./src/officerReport/router');
+const reportCategory = require('./src/category/router');
 
 const { decodeToken } = require('./src/auth/middleware');
 
@@ -39,6 +44,7 @@ app.use(reportRouter);
 app.use(routers);
 app.use(uploadImage);
 app.use(officerReportRouter);
+app.use(reportCategory);
 
 const server = app.listen(port, () => console.log(`server running at ${port}`));
 module.exports = server;
