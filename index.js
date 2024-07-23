ac3d4afdddc10eb2418a65b300df4e84254e69e4
Qois Abdul Qudus <qoisabdulquduss@gmail.com>
2024-07-23 11:32:06 +0700

    Back-End SerenityHub

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ app.use(express.static(path.join(__dirname, 'default')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const port = 5500;
 app.use(cors());
+app.use(morgan('combined', { stream: accessLogStream }));
+
+const express = require('express');
+const cors = require('cors');
 
 app.use(cors({
   origin: ['https://serenityhub.netlify.app/'],
